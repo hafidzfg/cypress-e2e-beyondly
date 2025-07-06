fe4b962f6d2f22e79e2da902f76060ae797d42cd
hafidzfg <hafidzfg@gmail.com>
2025-07-06 11:44:51 +0700

    feat: add test case script

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,6 +1,15 @@
-// test cases for register page
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io');
-  });
+import 'cypress-if';
+import { Given } from '@badeball/cypress-cucumber-preprocessor';
+import PromotionModal from '../pom/promotionModal';
+import Header from '../pom/header';
+
+const BASE_URL = Cypress.env('e2e').baseUrl;
+
+Given('As a user I open the homepage', () => {
+  cy.visit(Cypress.env('e2e').baseUrl);
+
+  // handling if shown promotion modal
+  PromotionModal.promotionCloseButton().if('visible').click();
+  Header.loginButton().click();
+  cy.url().should('eq', `${BASE_URL}/login`);
 });
