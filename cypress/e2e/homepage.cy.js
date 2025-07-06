1b40b475a49b2663264d95b7167b5c7c2a198b3f
hafidzfg <hafidzfg@gmail.com>
2025-07-06 12:14:16 +0700

    feat: add step def & set viewport

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,15 +1,18 @@
 import 'cypress-if';
-import { Given } from '@badeball/cypress-cucumber-preprocessor';
+import { Given, Then } from '@badeball/cypress-cucumber-preprocessor';
 import PromotionModal from '../pom/promotionModal';
 import Header from '../pom/header';
 
 const BASE_URL = Cypress.env('e2e').baseUrl;
 
 Given('As a user I open the homepage', () => {
+  cy.viewport(1366, 768);
   cy.visit(Cypress.env('e2e').baseUrl);
-
   // handling if shown promotion modal
   PromotionModal.promotionCloseButton().if('visible').click();
+});
+
+Then('I click the Masuk button', () => {
   Header.loginButton().click();
   cy.url().should('eq', `${BASE_URL}/login`);
 });
