895946216cc18729ce588288491803b04552f3a9
hafidzfg <hafidzfg@gmail.com>
2025-07-07 17:47:12 +0700

    feat: add pom related to main banner

--- cypress/pom/Homepage.js
+++ cypress/pom/Homepage.js
@@ -1,3 +1,29 @@
 export default class Homepage {
-  static;
+  static imgBannerMain() {
+    return cy.get('img[alt="banner-pict"]');
+  }
+  static buttonNext() {
+    return cy.get('button[class="slick-arrow slick-next"]');
+  }
+  static buttonPrevious() {
+    return cy.get('button[class="slick-arrow slick-prev"]');
+  }
+  static buttonCloseWa() {
+    return cy.get('button[aria-label="Close"]');
+  }
+  static textWa() {
+    return cy.contains('Kamu bisa menghubungi kami via whatsapp');
+  }
+  static linkSvgWa() {
+    return cy.get('[href="https://wa.me/6285934847324"]').children('svg');
+  }
+  static slickDotsContainer() {
+    return cy.get('ul.slick-dots');
+  }
+  static dotItems() {
+    return this.slickDotsContainer().children('li');
+  }
+  static activeDot() {
+    return this.slickDotsContainer().children('li.slick-active');
+  }
 }
