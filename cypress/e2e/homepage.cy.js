90d5b1d489e65c0765f6b1a8e0a2907048a33c12
hafidzfg <hafidzfg@gmail.com>
2025-07-07 19:49:07 +0700

    feat: add pom & assertion related to explanation section

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -11,6 +11,7 @@ Given('As a user I open the homepage', () => {
   cy.visit(Cypress.env('e2e').baseUrl);
   // handling if shown promotion modal
   PromotionModal.closeIfVisible();
+  // PromotionModal.promotionCloseButton().should('not.visible');
 });
 
 Then('I click the Masuk button', () => {
@@ -40,5 +41,16 @@ Then('I am shown the valid homepage details', () => {
   Homepage.slickDotsContainer().should('be.visible');
   Homepage.dotItems().should('be.visible').and('have.length', 4);
   Homepage.activeDot().should('be.visible').and('have.length', 1);
+  // Explanation section
+  Homepage.containerExplanation().scrollIntoView({ offset: { top: -200 } }, { duration: 2000 });
+  Homepage.imgLogoBeyondly().should('be.visible');
+  Homepage.textBeyondly().should('be.visible').and('have.text', '#ToLiveBeyond');
+  Homepage.textExplanationTitle().should('be.visible').and('have.text', 'Transform Yourself to be a Better You');
+  Homepage.textExplanationParagraph()
+    .should('be.visible')
+    .and(
+      'have.text',
+      "Beyondly doesn’t just fit into your life—it elevates it, effortlessly. It's woven into the way you live, work, and move through the day. At Beyondly, we believe wellness is not a destination. It is in every breath, every choice, and every movement, so you can live fully, breathe freely, and move with clarity."
+    );
   // Verify footer section
 });
--- cypress/pom/Homepage.js
+++ cypress/pom/Homepage.js
@@ -1,12 +1,13 @@
 export default class Homepage {
+  // Main banner section
   static imgBannerMain() {
     return cy.get('img[alt="banner-pict"]');
   }
   static buttonNext() {
-    return cy.get('button[class="slick-arrow slick-next"]');
+    return cy.get('button[class="slick-arrow slick-next"]').first();
   }
   static buttonPrevious() {
-    return cy.get('button[class="slick-arrow slick-prev"]');
+    return cy.get('button[class="slick-arrow slick-prev"]').first();
   }
   static buttonCloseWa() {
     return cy.get('button[aria-label="Close"]');
@@ -25,5 +26,28 @@ export default class Homepage {
   }
   static activeDot() {
     return this.slickDotsContainer().children('li.slick-active');
+  }
+  // Explanation below main banner
+  static containerExplanation() {
+    return cy.get('[class*="explanation-tab_about-container"]');
+  }
+  static containerExplanationAnimation() {
+    return this.containerExplanation().children('[class*="animate"]');
+  }
+  static imgLogoBeyondly() {
+    return cy.get('[class*="explanation-tab_header"]').children('img');
+  }
+  static textBeyondly() {
+    return cy.get('[class*="explanation-tab_header-text"]');
+  }
+  static textExplanationTitle() {
+    return cy.get('[class*="explanation-tab_p-1"]');
+  }
+  static textExplanationParagraph() {
+    return cy.get('[class*="explanation-tab_p-2"]');
+  }
+  // Showcase slider section
+  static containerShowcase() {
+    return cy.get('[class*="explanation-tab_about-container"').next('div.row');
   }
 }
